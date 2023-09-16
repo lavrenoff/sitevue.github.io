e18edfcc03565144d5e757ffaf831025fee22a2a
Лавренов Евгений <lavr020775@gmail.com>
2023-09-16 20:37:30 +0300

    init

---
+++ script.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: '.app',
+    data: {
+        items: [{
+            src: 'img/news1.png',
+            name: 'Let’s Get Solution For Building Construction Work',
+            dat: '26 December,2022',
+        }, {
+            src: 'img/news2.png',
+            name: 'Low Cost Latest Invented Interior Designing Ideas.',
+            dat: '22 December,2022',
+        },
+        {
+            src: 'img/news3.png',
+            name: 'Best For Any Office & Business Interior Solution',
+            dat: '25 December,2022',
+        },
+        {
+            src: 'img/news4.png',
+            name: 'Let’s Get Solution For Building Construction Work',
+            dat: '26 December,2022',
+        }, {
+            src: 'img/news5.png',
+            name: 'Low Cost Latest Invented Interior Designing Ideas.',
+            dat: '22 December,2022',
+        },
+        {
+            src: 'img/news6.png',
+            name: 'Best For Any Office & Business Interior Solution',
+            dat: '25 December,2022',
+        }
+        ]
+
+    }
+}
+)
+
