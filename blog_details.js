5965a17e8d9ea35771273f207a90c274d380b999
Лавренов Евгений <lavr020775@gmail.com>
2023-09-28 19:20:16 +0300

    init

---
+++ blog_details.js
@@ -0,0 +1,193 @@
+Vue.component('blogdetails-bedroom', {
+    template: `
+    <div class="blogdetails__txt1 blogdetails__txt2">
+    <img class="blogdetails__img blogdetails__img1"
+        src="img/blogdetails2.png" alt>
+    <p class="blogdetails__p1">
+        Lorem ipsum dolor sit amet, adipiscing
+        Aliquam eu sem vitae turpmaximus.posuere
+        in.Contrary to popular belief.There are many
+        variations of passages of Lorem Ipsum
+        available, but the majority have suffered.
+    </p>
+</div>
+`
+});
+
+Vue.component('blogdetails-building', {
+    template: `
+    <div class="blogdetails__txt1">
+        <h2 class="blogdetails__h1">
+            Design sprints are great
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, adipiscing
+            Aliquam eu sem vitae turpmaximus.posuere
+            in.Contrary to popular belief.There are many
+            variations of passages of Lorem Ipsum
+            available, but the majority have suffered.
+        </p>
+        <ol class="blogdetails__ol">
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+        </ol>
+    </div>`
+});
+
+Vue.component('blogdetails-architecture', {
+    template: `<div class="blogdetails__quote">
+    <svg width="74" height="56" viewBox="0 0 74 56"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"><path
+            d="M58.7516 0.600003C63.1516 0.600003 66.7516 2.06667 69.5516 5C72.2182 7.93334 73.5516 11.8667 73.5516 16.8C73.5516 25.4667 70.6849 33 64.9516 39.4C59.0849 45.8 52.0849 51.0667 43.9516 55.2L41.9516 51.8C45.2849 49.6667 48.4182 46.9333 51.3516 43.6C54.1516 40.2667 56.3516 36.6667 57.9516 32.8L52.3516 28.6C50.0849 27.1333 48.2182 25.1333 46.7516 22.6C45.2849 20.2 44.5516 17.8667 44.5516 15.6C44.5516 11.3333 45.8849 7.73334 48.5516 4.8C51.0849 2 54.4849 0.600003 58.7516 0.600003ZM17.5516 0.599999C21.9516 0.6 25.5516 2.06667 28.3516 5C31.0182 7.93333 32.3516 11.8667 32.3516 16.8C32.3516 25.4667 29.4849 33 23.7516 39.4C17.8849 45.8 10.8849 51.0667 2.75156 55.2L0.751558 51.8C4.08489 49.6667 7.21823 46.9333 10.1516 43.6C12.9516 40.2667 15.1516 36.6667 16.7516 32.8L11.1516 28.6C8.88489 27.1333 7.01823 25.1333 5.55156 22.6C4.08489 20.2 3.35156 17.8667 3.35156 15.6C3.35156 11.3333 4.6849 7.73333 7.35156 4.8C9.8849 2 13.2849 0.599999 17.5516 0.599999Z"
+            fill="#CDA274"></path></svg>
+    <p class="blogdetails__t1">The details are not
+        the details.
+    </p>
+    <p class="blogdetails__t2">
+        They make the design.
+    </p>
+</div>`
+});
+
+Vue.component('blogdetails-kitchen_planning', {
+    template: `<div class="blogdetails__txt1">
+    <h2 class="blogdetails__h1">
+        Design sprints are great
+    </h2>
+    <p>
+        Lorem ipsum dolor sit amet, adipiscing
+        Aliquam eu sem vitae turpmaximus.posuere
+        in.Contrary to popular belief.There are many
+        variations of passages of Lorem Ipsum
+        available, but the majority have suffered.
+    </p>
+    <ol class="blogdetails__ol">
+        <li>Contrary to popular belief.There are
+            many variations of passages of Lorem
+            Ipsum available, but the majority have
+            suffered.</li>
+        <li>Contrary to popular belief.There are
+            many variations of passages of Lorem
+            Ipsum available, but the majority have
+            suffered.</li>
+        <li>Contrary to popular belief.There are
+            many variations of passages of Lorem
+            Ipsum available, but the majority have
+            suffered.</li>
+    </ol>
+</div>`
+});
+
+Vue.component('blogdetails-kitchen', {
+    template: `<article>
+    <h1 class="blogdetails__h1">
+        Let’s Get Solution for Building
+        Construction Work
+    </h1>
+    <img class="blogdetails__img"
+        src="img/blogdetails1.png" alt>
+    <div class="blogdetails__date">
+        <span>
+            26 December,2022
+        </span>
+        <span>
+            Interior / Home / Decore
+        </span>
+    </div>
+
+    <div class="blogdetails__txt1">
+        <p>Lorem ipsum dolor sit amet, adipiscing
+            Aliquam eu sem vitae turpmaximus.posuere
+            in.Contrary to popular belief.There are many
+            variations of passages of Lorem Ipsum
+            available, but the majority have suffered
+            alteration in some form, by injecthumour, or
+            randomised words which don't look even
+            slightly believable.<br><br>
+            Embarrassing hidden in the middle of text.
+            All the Lorem Ipsum generators on the
+            Internet tend to repeat predefined chunks as
+            necessary.</p>
+    </div>
+
+    <div class="blogdetails__quote">
+        <svg width="74" height="56" viewBox="0 0 74 56"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"><path
+                d="M58.7516 0.600003C63.1516 0.600003 66.7516 2.06667 69.5516 5C72.2182 7.93334 73.5516 11.8667 73.5516 16.8C73.5516 25.4667 70.6849 33 64.9516 39.4C59.0849 45.8 52.0849 51.0667 43.9516 55.2L41.9516 51.8C45.2849 49.6667 48.4182 46.9333 51.3516 43.6C54.1516 40.2667 56.3516 36.6667 57.9516 32.8L52.3516 28.6C50.0849 27.1333 48.2182 25.1333 46.7516 22.6C45.2849 20.2 44.5516 17.8667 44.5516 15.6C44.5516 11.3333 45.8849 7.73334 48.5516 4.8C51.0849 2 54.4849 0.600003 58.7516 0.600003ZM17.5516 0.599999C21.9516 0.6 25.5516 2.06667 28.3516 5C31.0182 7.93333 32.3516 11.8667 32.3516 16.8C32.3516 25.4667 29.4849 33 23.7516 39.4C17.8849 45.8 10.8849 51.0667 2.75156 55.2L0.751558 51.8C4.08489 49.6667 7.21823 46.9333 10.1516 43.6C12.9516 40.2667 15.1516 36.6667 16.7516 32.8L11.1516 28.6C8.88489 27.1333 7.01823 25.1333 5.55156 22.6C4.08489 20.2 3.35156 17.8667 3.35156 15.6C3.35156 11.3333 4.6849 7.73333 7.35156 4.8C9.8849 2 13.2849 0.599999 17.5516 0.599999Z"
+                fill="#CDA274"></path></svg>
+        <p class="blogdetails__t1">The details are not
+            the details.
+        </p>
+        <p class="blogdetails__t2">
+            They make the design.
+        </p>
+    </div>
+
+    <div class="blogdetails__txt1">
+        <h2 class="blogdetails__h1">
+            Design sprints are great
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, adipiscing
+            Aliquam eu sem vitae turpmaximus.posuere
+            in.Contrary to popular belief.There are many
+            variations of passages of Lorem Ipsum
+            available, but the majority have suffered.
+        </p>
+        <ol class="blogdetails__ol">
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+            <li>Contrary to popular belief.There are
+                many variations of passages of Lorem
+                Ipsum available, but the majority have
+                suffered.</li>
+        </ol>
+    </div>
+
+    <div class="blogdetails__txt1 blogdetails__txt2">
+        <img class="blogdetails__img blogdetails__img1"
+            src="img/blogdetails2.png" alt>
+        <p class="blogdetails__p1">
+            Lorem ipsum dolor sit amet, adipiscing
+            Aliquam eu sem vitae turpmaximus.posuere
+            in.Contrary to popular belief.There are many
+            variations of passages of Lorem Ipsum
+            available, but the majority have suffered.
+        </p>
+    </div>
+</article>
+`
+});
+
+new Vue({
+    el: '.blogdetails',
+    data: {
+        buttons: [{ id: 'kitchen', text: 'Kitchen' }, { id: 'bedroom', text: 'Bedroom' }, { id: 'building', text: 'Building' }, { id: 'architecture', text: 'Architecture' }, { id: 'kitchen_planning', text: 'Kitchen Planing' }],
+        currentTab: 'kitchen'
+    },
+
+    computed: {
+        changeTabs() {
+            return `blogdetails-${this.currentTab}`;
+        }
+    }
+
+})
